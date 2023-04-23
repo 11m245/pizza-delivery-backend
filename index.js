29edf4f6540f5b58d9f87c7d6170bf318f94658b
11m245 <sivaraj2siva@gmail.com>
2023-04-23 22:14:25 +0530

    cors only

--- index.js
+++ index.js
@@ -32,8 +32,8 @@ import paymentsRouter from "./routes/payments.route.js";
 const app = express();
 const PORT = process.env.PORT;
 // app.use(express.json());
-// app.use(cors());
-app.use(cors({ origin: process.env.API_CLIENT, credentials: true }));
+app.use(cors());
+// app.use(cors({ origin: process.env.API_CLIENT, credentials: true }));
 
 //mongo connection
 
