4d214aeb8830328806b97d3469fadcbb066ae65a
11m245 <sivaraj2siva@gmail.com>
2023-04-23 22:55:46 +0530

    update

--- services/payments.service.js
+++ services/payments.service.js
@@ -1,5 +1,7 @@
 import { ObjectId } from "mongodb";
 import { client } from "../index.js";
+import * as dotenv from "dotenv";
+dotenv.config();
 export async function addOrderInPayments(data) {
   return await client
     .db("pizzaDeliveryApp")
@@ -17,22 +19,29 @@ export async function updateOrderPaymentStatus({
   paymentStatus,
   paymentIntent,
 }) {
-  console.log("update payment runss");
-  return await client
-    .db("pizzaDeliveryApp")
-    .collection("payments")
-    .updateOne(
-      { orderId: ObjectId(orderIdString) },
-      {
-        $set: {
-          modeOfPayment: "stripe",
-          checkOutSessionId: checkOutSessionId,
-          paidAmount: paidAmount,
-          updatedAt: updatedAt,
-          sessionStatus: sessionStatus,
-          paymentStatus: paymentStatus,
-          paymentIntent: paymentIntent,
-        },
-      }
-    );
+  try {
+    const client = new MongoClient(process.env.MONGO_URL);
+    client.connect();
+
+    console.log("update payment runss");
+    return await client
+      .db("pizzaDeliveryApp")
+      .collection("payments")
+      .updateOne(
+        { orderId: ObjectId(orderIdString) },
+        {
+          $set: {
+            modeOfPayment: "stripe",
+            checkOutSessionId: checkOutSessionId,
+            paidAmount: paidAmount,
+            updatedAt: updatedAt,
+            sessionStatus: sessionStatus,
+            paymentStatus: paymentStatus,
+            paymentIntent: paymentIntent,
+          },
+        }
+      );
+  } catch (err) {
+    console.log("error in update payment in db by hook", err);
+  }
 }
